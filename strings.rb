cfb342ce437e08947bd884ea7407940b0eb88870
ValentynaGorbachenko <gorbachenko.valentyna@gmail.com>
2017-02-04 12:38:28 -0800

    add strings.rb

---
+++ strings.rb
@@ -0,0 +1,22 @@
+puts "Strings practice"
+
+puts "I am a string.class => " + "I am a string".class.to_s
+
+puts "\"foo = \%{foo}\" \% {:foo => 'bar'} => " + "foo = %{foo}" % {:foo => 'bar'}
+
+puts "hello from + self.to_s => " +"hello from "+ self.to_s
+
+puts "'string'*5 => "+ "Strings"*5
+
+str = "valia"
+puts "str = \"valia\""
+
+puts "str.capitalize => "+ str.capitalize
+puts "str => " + str
+
+puts "str.capitalize! => "+ str.capitalize!
+puts "str => " + str
+
+puts "str<<str => " +str<<str
+
+puts "str.concat('5') =>"+str.concat('5')+ " and str is #{str}"
